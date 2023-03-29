685990e04b23310d92bf1b7990795001c7bd395f
nczala <91031963+nczala@users.noreply.github.com>
2023-03-29 18:24:07 +0200

    creation & styling of lobbyOverview page #3

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -1,8 +1,9 @@
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
-import {GameGuard} from "components/routing/routeProtectors/GameGuard";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { GameGuard } from "components/routing/routeProtectors/GameGuard";
 import GameRouter from "components/routing/routers/GameRouter";
-import {LoginGuard} from "components/routing/routeProtectors/LoginGuard";
+import { LoginGuard } from "components/routing/routeProtectors/LoginGuard";
 import Login from "components/views/Login";
+import LobbyOverview from "components/views/LobbyOverview";
 
 /**
  * Main router of your application.
@@ -19,16 +20,19 @@ const AppRouter = () => {
       <Switch>
         <Route path="/game">
           <GameGuard>
-            <GameRouter base="/game"/>
+            <GameRouter base="/game" />
           </GameGuard>
         </Route>
         <Route exact path="/login">
           <LoginGuard>
-            <Login/>
+            <Login />
           </LoginGuard>
         </Route>
         <Route exact path="/">
-          <Redirect to="/game"/>
+          <Redirect to="/game" />
+        </Route>
+        <Route exact path="/lobbies">
+          <LobbyOverview />
         </Route>
       </Switch>
     </BrowserRouter>
@@ -36,6 +40,6 @@ const AppRouter = () => {
 };
 
 /*
-* Don't forget to export your component!
+ * Don't forget to export your component!
  */
 export default AppRouter;
---
+++ src/components/views/LobbyOverview.js
@@ -0,0 +1,83 @@
+import "styles/views/LobbyOverview.scss";
+import { useHistory } from "react-router-dom";
+
+const LobbyItem = ({ lobby, handleClick }) => {
+  return (
+    <div className="sub-container sub-container-list list">
+      <div
+        onClick={() => handleClick(lobby)}
+        className="sub-container sub-container-list list name"
+      >
+        {lobby.lobbyName}
+      </div>
+      <div className="sub-container sub-container-list list size">
+        {lobby.lobbySize}
+      </div>
+    </div>
+  );
+};
+
+const LobbyOverview = () => {
+  const history = useHistory();
+
+  const lobbies = {
+    object1: {
+      lobbyName: "UZH",
+      lobbySize: 2,
+    },
+  };
+
+  const lobbyArray = Object.values(lobbies);
+
+  function handleClick(lobby) {
+    if (window.confirm(`join ${lobby.lobbyName} ?`)) {
+      // Perform the action here
+    }
+  }
+
+  const navigateToLobbySettings = () => {
+    history.push("/lobbysettings");
+  };
+
+  return (
+    <div className="main-container">
+      <div className="sub-container sub-container-header">
+        <h1>Lobby Overview</h1>
+        <h2>Join a lobby by clicking on the name</h2>
+      </div>
+      <div className="sub-container sub-container-list">
+        <div className="sub-container sub-container-list header">
+          <div className="sub-container sub-container-list header name">
+            Lobbies
+          </div>
+          <div className="sub-container sub-container-list header size">
+            Group Size
+          </div>
+        </div>
+        {lobbyArray.map((lobby) => (
+          <LobbyItem
+            lobby={lobby}
+            handleClick={handleClick}
+            key={lobby.lobbyName}
+          ></LobbyItem>
+        ))}
+      </div>
+      <div className="sub-container sub-container-buttons">
+        <button className="sub-container sub-container-buttons button one">
+          REFRESH
+        </button>
+        <button
+          onClick={navigateToLobbySettings}
+          className="sub-container sub-container-buttons button two"
+        >
+          CREATE A NEW LOBBY
+        </button>
+        <button className="sub-container sub-container-buttons button three">
+          LOGOUT
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LobbyOverview;
