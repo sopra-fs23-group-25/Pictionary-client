e93c4a4f14918e6e5dab0d30f119f831cf0a1d79
Nico Zala <nicocamillo.zala@uzh.ch>
2023-04-19 11:55:40 +0200

    deployment fixes

--- src/components/views/EndOfGame.js
+++ src/components/views/EndOfGame.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
-import "styles/views/EndOfGame.scss"
+import "styles/views/EndOfGame.scss";
 //import "styles/views/Login.scss"
-import {api, handleError} from "../../helpers/api";
-import {Spinner} from "../ui/Spinner";
+import { handleError } from "../../helpers/api";
+import { Spinner } from "../ui/Spinner";
 
 /*
 It is possible to add multiple components inside a single file,
@@ -18,172 +18,153 @@ MOCKS
  */
 
 class User {
-    constructor(data = {}) {
-        this.position = null;
-        this.username = null;
-        this.points = null;
-        Object.assign(this, data);
-    }
+  constructor(data = {}) {
+    this.position = null;
+    this.username = null;
+    this.points = null;
+    Object.assign(this, data);
+  }
 }
 
-
-const UserItem = ({user}) => {
-    return (
-            <div className="sub-container sub-container-list list">
-                <div
-                        className="sub-container sub-container-list list position"
-                >
-                    {user.position}
-                </div>
-                <div
-                        className="sub-container sub-container-list list username"
-                >
-                    {user.username}
-                </div>
-                <div className="sub-container sub-container-list list points">
-                    {user.points}
-                </div>
-            </div>
-    );
+const UserItem = ({ user }) => {
+  return (
+    <div className="sub-container sub-container-list list">
+      <div className="sub-container sub-container-list list position">
+        {user.position}
+      </div>
+      <div className="sub-container sub-container-list list username">
+        {user.username}
+      </div>
+      <div className="sub-container sub-container-list list points">
+        {user.points}
+      </div>
+    </div>
+  );
 };
 
-const EndOfGame = (props) => {
-
-    /*
-MOCKS
- */
-    var mockusers = [];
-    const u1 = new User();
-    u1.username = "Mionel Lessi";
-    u1.position = "#1";
-    u1.points = "210";
-    mockusers.push(u1);
-
-    const u2 = new User();
-    u2.username = "Foger Rederer";
-    u2.position = "#2";
-    u2.points = "160";
-    mockusers.push(u2);
-
-    const u3 = new User();
-    u3.username = "The Two";
-    u3.position = "#3";
-    u3.points = "120";
-    mockusers.push(u3);
-
-    const u4 = new User();
-    u4.username = "The Two";
-    u4.position = "#4";
-    u4.points = "120";
-    mockusers.push(u4);
-
-    const u5 = new User();
-    u5.username = "LittleBigSmall";
-    u5.position = "#5";
-    u5.points = "90";
-    mockusers.push(u5);
-    mockusers.push(u5);
-    mockusers.push(u5);
-
-
-
-
-
-
-    /*
-MOCKS
- */
-
+const EndOfGame = () => {
   const history = useHistory();
   const [users, setUsers] = useState(null);
 
-    const fetchData = async () => {
-        try {
-            //URL NOCH ANPASSEN
-            //const response = await api.get("/lobbies/{lobbyId}/end");
-            //setUsers(response.data);
-            setUsers(mockusers);
-        } catch (error) {
-            console.error(
-                    `Something went wrong while fetching the lobbies: \n${handleError(
-                            error
-                    )}`
-            );
-        }
-    };
+  const fetchData = async () => {
+    try {
+      //URL NOCH ANPASSEN
+      //const response = await api.get("/lobbies/{lobbyId}/end");
+      //setUsers(response.data);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+      /*
+MOCKS
+ */
+      var mockusers = [];
+      const u1 = new User();
+      u1.username = "Mionel Lessi";
+      u1.position = "#1";
+      u1.points = "210";
+      mockusers.push(u1);
 
-    let userListContent = <Spinner></Spinner>;
-    if (users) {
-        userListContent = users.map((user) => (
-                <UserItem
-                        user={user}
-                        //handleClick={handleClickOnLobby}
-                        key={user.username}
-                        //points={user.points}
+      const u2 = new User();
+      u2.username = "Foger Rederer";
+      u2.position = "#2";
+      u2.points = "160";
+      mockusers.push(u2);
 
-                ></UserItem>
-        ));
+      const u3 = new User();
+      u3.username = "The Two";
+      u3.position = "#3";
+      u3.points = "120";
+      mockusers.push(u3);
+
+      const u4 = new User();
+      u4.username = "The TwoWE";
+      u4.position = "#4";
+      u4.points = "120";
+      mockusers.push(u4);
+
+      const u5 = new User();
+      u5.username = "LittleBigSmall";
+      u5.position = "#5";
+      u5.points = "90";
+      mockusers.push(u5);
+
+      /*
+MOCKS
+ */
+
+      setUsers(mockusers);
+    } catch (error) {
+      console.error(
+        `Something went wrong while fetching the lobbies: \n${handleError(
+          error
+        )}`
+      );
     }
+  };
 
-    const startNewGame = () => {
-        //api.delete("/games/{lobbyId}");
-        //api.post("/games");
-        history.push("/endofturn");
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const exit = () => {
-        //api.delete("/games/{lobbyId}");
-        history.push("/endofturn");
-    };
+  let userListContent = <Spinner></Spinner>;
+  if (users) {
+    userListContent = users.map((user) => (
+      <UserItem
+        user={user}
+        //handleClick={handleClickOnLobby}
+        key={user.username}
+        //points={user.points}
+      ></UserItem>
+    ));
+  }
+
+  const startNewGame = () => {
+    //api.delete("/games/{lobbyId}");
+    //api.post("/games");
+    history.push("/endofturn");
+  };
+
+  const exit = () => {
+    //api.delete("/games/{lobbyId}");
+    history.push("/endofturn");
+  };
 
   return (
     <BaseContainer>
-        <div className="popup-endofgame">
-            <div className="trophy">
+      <div className="popup-endofgame">
+        <div className="trophy"></div>
+        <div className="sub-container">
+          <div className="sub-container header-container">
+            <h1>Final Scores</h1>
+          </div>
 
+          <div className="sub-container sub-container-list">
+            <div className="sub-container sub-container-list header">
+              <div className="sub-container sub-container-list header position">
+                Position
+              </div>
+              <div className="sub-container sub-container-list header name">
+                Username
+              </div>
+              <div className="sub-container sub-container-list header points">
+                Final Points
+              </div>
             </div>
-            <div className="sub-container">
-                <div className="sub-container header-container">
-                    <h1>
-                        Final Scores
-                    </h1>
-                </div>
-
-                <div className="sub-container sub-container-list">
-                    <div className="sub-container sub-container-list header">
-                        <div className="sub-container sub-container-list header position">
-                            Position
-                        </div>
-                        <div className="sub-container sub-container-list header name">
-                            Username
-                        </div>
-                        <div className="sub-container sub-container-list header points">
-                            Final Points
-                        </div>
-                    </div>
-                    {userListContent}
-                </div>
-            </div>
-            <div className="flag">
-            </div>
-            <div className="button-container">
-
-                <div className="button-container button-game-again"
-                     onClick={() => startNewGame()}>
-                    Game again?
-                </div>
-
-                <div className="button-container button-exit"
-                     onClick={() => exit()}>
-                    Exit
-                </div>
-                </div>
-
+            {userListContent}
+          </div>
         </div>
+        <div className="flag"></div>
+        <div className="button-container">
+          <div
+            className="button-container button-game-again"
+            onClick={() => startNewGame()}
+          >
+            Game again?
+          </div>
+
+          <div className="button-container button-exit" onClick={() => exit()}>
+            Exit
+          </div>
+        </div>
+      </div>
     </BaseContainer>
   );
 };
--- src/components/views/EndOfTurnResults.js
+++ src/components/views/EndOfTurnResults.js
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from "react";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
-import "styles/views/EndOfTurnResults.scss"
-import "styles/views/Login.scss"
-import {api, handleError} from "../../helpers/api";
-import {Spinner} from "../ui/Spinner";
+import "styles/views/EndOfTurnResults.scss";
+import "styles/views/Login.scss";
+import { handleError } from "../../helpers/api";
+import { Spinner } from "../ui/Spinner";
 //import { setTimeout } from "timers/promises";
-
 
 /*
 It is possible to add multiple components inside a single file,
@@ -20,174 +19,151 @@ MOCKS
  */
 
 class User {
-    constructor(data = {}) {
-        this.position = null;
-        this.username = null;
-        this.points = null;
-        this.word = null;
-        Object.assign(this, data);
-    }
+  constructor(data = {}) {
+    this.position = null;
+    this.username = null;
+    this.points = null;
+    this.word = null;
+    Object.assign(this, data);
+  }
 }
 
-
-const UserItem = ({user}) => {
-    return (
-            <div className="sub-container-players sub-container-list list">
-                <div
-                        className="sub-container-players sub-container-list list position"
-                >
-                    {user.position}
-                </div>
-                <div
-                        className="sub-container-players sub-container-list list name"
-                >
-                    {user.username}
-                </div>
-                <div className="sub-container-players sub-container-list list points">
-                    {user.points}
-                </div>
-                <div className="sub-container-players sub-container-list list guessed-word"
-                >
-
-                    {user.word}
-                </div>
-            </div>
-    );
+const UserItem = ({ user }) => {
+  return (
+    <div className="sub-container-players sub-container-list list">
+      <div className="sub-container-players sub-container-list list position">
+        {user.position}
+      </div>
+      <div className="sub-container-players sub-container-list list name">
+        {user.username}
+      </div>
+      <div className="sub-container-players sub-container-list list points">
+        {user.points}
+      </div>
+      <div className="sub-container-players sub-container-list list guessed-word">
+        {user.word}
+      </div>
+    </div>
+  );
 };
 
 const EndOfTurn = (props) => {
-
-    /*
-MOCKS
- */
-    var mockusers = [];
-    const u1 = new User();
-    u1.username = "Mionel Lessi";
-    u1.position = "#1";
-    u1.points = "+50";
-    u1.word = "duck";
-    mockusers.push(u1);
-
-    const u2 = new User();
-    u2.username = "Foger Rederer";
-    u2.position = "#2";
-    u2.points = "+30";
-    u2.word = "duck";
-    mockusers.push(u2);
-
-    const u3 = new User();
-    u3.username = "The Two";
-    u3.position = "#3";
-    u3.points = "+0";
-    u3.word = "";
-    mockusers.push(u3);
-
-    const u4 = new User();
-    u4.username = "The Two";
-    u4.position = "#4";
-    u4.points = "+0";
-    u4.word = "bird";
-    mockusers.push(u4);
-
-    const u5 = new User();
-    u5.username = "LittleBigSmall";
-    u5.position = "#5";
-    u5.points = "+0";
-    u5.word = "dog";
-    mockusers.push(u5);
-    mockusers.push(u5);
-    mockusers.push(u5);
-    mockusers.push(u5);
-    mockusers.push(u5);
-
-
-    /*
-MOCKS
- */
-
-  const history = useHistory();
+  // const history = useHistory();
   const [users, setUsers] = useState(null);
-  function sleep(ms) {
-        return new Promise(val => setTimeout(val, ms));
+
+  const fetchData = async () => {
+    try {
+      //URL NOCH ANPASSEN
+      //const response_users = await api.get("/lobbies/{lobbyId}/users");
+      //const response_guesses = await api.get("/games/{lobbyId}/playerguess");
+      //setUsers(response_users.data);
+      //setGuesses(response_guesses.data);
+      /*
+MOCKS
+ */
+      var mockusers = [];
+      const u1 = new User();
+      u1.username = "Mionel Lessi";
+      u1.position = "#1";
+      u1.points = "+50";
+      u1.word = "duck";
+      mockusers.push(u1);
+
+      const u2 = new User();
+      u2.username = "Foger Rederer";
+      u2.position = "#2";
+      u2.points = "+30";
+      u2.word = "duck";
+      mockusers.push(u2);
+
+      const u3 = new User();
+      u3.username = "The Two";
+      u3.position = "#3";
+      u3.points = "+0";
+      u3.word = "";
+      mockusers.push(u3);
+
+      const u4 = new User();
+      u4.username = "The Twasaso";
+      u4.position = "#4";
+      u4.points = "+0";
+      u4.word = "bird";
+      mockusers.push(u4);
+
+      const u5 = new User();
+      u5.username = "LittleBigSmall";
+      u5.position = "#5";
+      u5.points = "+0";
+      u5.word = "dog";
+      mockusers.push(u5);
+
+      /*
+MOCKS
+ */
+      setUsers(mockusers);
+      //await sleep(1000);
+      //document.getElementById("base-container").style.visibility = "hidden";
+    } catch (error) {
+      console.error(
+        `Something went wrong while fetching the lobbies: \n${handleError(
+          error
+        )}`
+      );
     }
+  };
 
-    const fetchData = async () => {
-        try {
-            //URL NOCH ANPASSEN
-            //const response_users = await api.get("/lobbies/{lobbyId}/users");
-            //const response_guesses = await api.get("/games/{lobbyId}/playerguess");
-            //setUsers(response_users.data);
-            //setGuesses(response_guesses.data);
-            setUsers(mockusers);
-            //await sleep(1000);
-            //document.getElementById("base-container").style.visibility = "hidden";
-        } catch (error) {
-            console.error(
-                    `Something went wrong while fetching the lobbies: \n${handleError(
-                            error
-                    )}`
-            );
-        }
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    let userListContent = <Spinner></Spinner>;
-    if (users) {
-        userListContent = users.map((user) => (
-                <UserItem
-                        user={user}
-                        //handleClick={handleClickOnLobby}
-                        key={user.username}
-                        //points={user.points}
-
-                ></UserItem>
-        ));
-    }
-
+  let userListContent = <Spinner></Spinner>;
+  if (users) {
+    userListContent = users.map((user) => (
+      <UserItem
+        user={user}
+        //handleClick={handleClickOnLobby}
+        key={user.username}
+        //points={user.points}
+      ></UserItem>
+    ));
+  }
 
   return (
     <BaseContainer id="base-container">
-        <div className="popup">
-            <div className="sub-container-players">
-                <div className="sub-container-players header-container">
-                    <h1>
-                        Results of Round 3
-                    </h1>
-                </div>
-                <div className="sub-container-players sub-container-list">
-                    <div className="sub-container-players sub-container-list header">
-                        <div className="sub-container-players sub-container-list header position">
-                            Position
-                        </div>
-                        <div className="sub-container-players sub-container-list header name">
-                            Username
-                        </div>
-                        <div className="sub-container-players sub-container-list header points">
-                            New Points
-                        </div>
-                        <div className="sub-container-players sub-container-list header guessed-word">
-                            Guessed Word
-                        </div>
-                    </div>
-                    {userListContent}
-                </div>
-                <div className = "sub-container-players flag-container">
-
-                </div>
-                <div className="sub-container-players header-nextRound">
-                    Get ready for Round 4!!!
-                </div>
-                <div className="sub-container-players header-nextPainter">
-                    New Painter: Mionel Lessi.
-                </div>
-                <div className="sub-container-players header-correct-word">
-                    The word was: duck.
-                </div>
+      <div className="popup">
+        <div className="sub-container-players">
+          <div className="sub-container-players header-container">
+            <h1>Results of Round 3</h1>
+          </div>
+          <div className="sub-container-players sub-container-list">
+            <div className="sub-container-players sub-container-list header">
+              <div className="sub-container-players sub-container-list header position">
+                Position
+              </div>
+              <div className="sub-container-players sub-container-list header name">
+                Username
+              </div>
+              <div className="sub-container-players sub-container-list header points">
+                New Points
+              </div>
+              <div className="sub-container-players sub-container-list header guessed-word">
+                Guessed Word
+              </div>
             </div>
+            {userListContent}
+          </div>
+          <div className="sub-container-players flag-container"></div>
+          <div className="sub-container-players header-nextRound">
+            Get ready for Round 4!!!
+          </div>
+          <div className="sub-container-players header-nextPainter">
+            New Painter: Mionel Lessi.
+          </div>
+          <div className="sub-container-players header-correct-word">
+            The word was: duck.
+          </div>
         </div>
+      </div>
     </BaseContainer>
   );
 };
