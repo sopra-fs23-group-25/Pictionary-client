d0382a5670f5c3a89c2dde2036e976826ad04c4a
pchaco <pablo.chaconpino@uzh.ch>
2023-04-26 12:06:32 +0200

    - #42, #44, #45 get correct data of each turn and display it correctly

--- src/components/views/Game/EndOfTurnResults.js
+++ src/components/views/Game/EndOfTurnResults.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // import { useHistory } from "react-router-dom";
 //import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/Game/EndOfTurnResults.scss";
 import "styles/views/Login.scss";
-import { handleError } from "../../../helpers/api";
+//import { handleError } from "../../../helpers/api";
 import { Spinner } from "../../ui/Spinner";
 //import { setTimeout } from "timers/promises";
 
@@ -18,114 +18,43 @@ specific components that belong to the main one in the same file.
 MOCKS
  */
 
-class User {
-  constructor(data = {}) {
-    this.position = null;
-    this.username = null;
-    this.points = null;
-    this.guess = null;
-    Object.assign(this, data);
-  }
-}
-
-const UserItem = ({ user }) => {
+const UserItem = ({ guess , index}) => {
   return (
     <div className="sub-container-players sub-container-list list">
       <div className="sub-container-players sub-container-list list position">
-        {user.position}
+        #{index+1}
       </div>
       <div className="sub-container-players sub-container-list list name">
-        {user.username}
+        {guess.username}
       </div>
       <div className="sub-container-players sub-container-list list points">
-        {user.points}
+        {guess.score}
       </div>
       <div className="sub-container-players sub-container-list list guessed-word">
-        {user.guess}
+        {guess.guess}
       </div>
     </div>
   );
 };
 
-const EndOfTurn = () => {
-  // const history = useHistory();
-  const [users, setUsers] = useState(null);
+const EndOfTurn = ({players, roundResult, currentRound, word}) => {
 
-  const fetchData = async () => {
-    try {
-      //URL NOCH ANPASSEN
-      //const response = await api.get("/lobbies/{lobbyId}/game/turnresult");
-      //setUsers(response.data);
-      //setGuesses(response.data);
-      /*
-MOCKS
- */
-      var mockusers = [];
-      const u1 = new User();
-      u1.username = "Mionel Lessi";
-      u1.position = "#1";
-      u1.points = "+50";
-      u1.guess = "duck";
-      mockusers.push(u1);
-
-      const u2 = new User();
-      u2.username = "Foger Rederer";
-      u2.position = "#2";
-      u2.points = "+30";
-      u2.guess = "duck";
-      mockusers.push(u2);
-
-      const u3 = new User();
-      u3.username = "The Two";
-      u3.position = "#3";
-      u3.points = "+0";
-      u3.guess = "";
-      mockusers.push(u3);
-
-      const u4 = new User();
-      u4.username = "The Twasaso";
-      u4.position = "#4";
-      u4.points = "+0";
-      u4.guess = "bird";
-      mockusers.push(u4);
-
-      const u5 = new User();
-      u5.username = "LittleBigSmall";
-      u5.position = "#5";
-      u5.points = "+0";
-      u5.guess = "dog";
-      mockusers.push(u5);
-
-      /*
-MOCKS
- */
-      setUsers(mockusers);
-      //await sleep(1000);
-      //document.getElementById("base-container").style.visibility = "hidden";
-    } catch (error) {
-      console.error(
-        `Something went wrong while fetching the lobbies: \n${handleError(
-          error
-        )}`
-      );
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  let userListContent = <Spinner></Spinner>;
-  if (users) {
-    userListContent = users.map((user) => (
+    let users = <Spinner></Spinner>;
+  if (roundResult.length > 0) {
+    users = roundResult.map((guess, index) => (
       <UserItem
-        user={user}
-        //handleClick={handleClickOnLobby}
-        key={user.username}
-        //points={user.points}
+        guess={guess}
+        index={index}
       ></UserItem>
     ));
   }
+
+
+
+
+    const painter = players.find((user) => user.currentRole === "PAINTER").username;
+    //const word = word;
+    //const nextRound = currentRound + 1;
 
   return (
     <div id="base-container-end-of-turn"
@@ -133,7 +62,7 @@ MOCKS
       <div className="popup">
         <div className="sub-container-players">
           <div className="sub-container-players header-container">
-            <h1>Results of Round 3</h1>
+            <h1>Results of Round {currentRound+1}</h1>
           </div>
           <div className="sub-container-players sub-container-list">
             <div className="sub-container-players sub-container-list header">
@@ -150,17 +79,17 @@ MOCKS
                 Guessed Word
               </div>
             </div>
-            {userListContent}
+              {users}
           </div>
           <div className="sub-container-players flag-container"></div>
           <div className="sub-container-players header-nextRound">
-            Get ready for Round 4!!!
+            Get ready for Round {currentRound+2}!!!
           </div>
           <div className="sub-container-players header-nextPainter">
-            New Painter: Mionel Lessi.
+            New Painter: {painter}
           </div>
           <div className="sub-container-players header-correct-word">
-            The word was: duck.
+            The word was: {word}.
           </div>
         </div>
       </div>
--- src/components/views/Game/Game.js
+++ src/components/views/Game/Game.js
@@ -47,6 +47,7 @@ const GameView = (props) => {
   const [currentRound, setCurrentRound] = useState(0);
 
   const [roundResult, setRoundResult] = useState([]);
+  const [word, setWord] = useState("");
 
   useEffect(() => {
     // this information was passed while creating/joining lobby
@@ -246,6 +247,7 @@ const GameView = (props) => {
   const startGame = async () => {
     // show startGame Component
     timerRef.current.startGame(10);
+      //timerRef.current.startGame(5);
     // GET Roles
     const gameResponse = await fetchGame();
     console.log("fetch game", gameResponse);
@@ -258,6 +260,7 @@ const GameView = (props) => {
 
   async function startRound() {
     timerRef.current.startRound(timePerRound);
+    //  timerRef.current.startRound(5);
     if (isPainter) {
       const response = await fetchTurn(); // to get word
       console.log("turn response", response);
@@ -272,15 +275,18 @@ const GameView = (props) => {
     // isEndOfRound: true
     //setIsEndOfRound(true);
     timerRef.current.endRound(10);
+    //  timerRef.current.endRound(30);
     // show Round Result
     // GET ROUND RESULT (roles, word)
     const turnResponse = await fetchTurn(); // to get Result
     const gameResponse = await fetchGame(); // to get new Roles
     console.log("turn response:", turnResponse);
     console.log("game response:", gameResponse);
+    //setRoundResult(turnResponse.data);
     setRoundResult(turnResponse.data.guesses.sort((a, b) => b.score - a.score));
     setPlayers(gameResponse.data.players.sort((a, b) => b.score - a.score));
     setCurrentRound(gameResponse.data.nrOfRoundsPlayed);
+    setWord(turnResponse.data.word);
     //.sort((a, b) => b.score - a.score);
     //configurePainter();
     // setWord, setPlayers
@@ -355,7 +361,12 @@ const GameView = (props) => {
               case "start game":
                 return <BeforeGameStart></BeforeGameStart>;
               case "end round":
-                return <EndOfTurn roundResult={roundResult}></EndOfTurn>;
+                return <EndOfTurn
+                        roundResult={roundResult}
+                        players={players}
+                        currentRound={currentRound}
+                        word={word}
+                ></EndOfTurn>;
               default:
                 return (
                   <DrawingBoard
