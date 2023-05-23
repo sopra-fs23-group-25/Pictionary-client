6c31a96f907634cec9eaf47015c5534f44c88f62
Nico Zala <nicocamillo.zala@uzh.ch>
2023-05-23 14:43:25 +0200

    english and german translation of rules-page

--- src/components/views/Rules.js
+++ src/components/views/Rules.js
@@ -1,71 +1,77 @@
 import { useHistory } from "react-router-dom";
 import "styles/views/Rules.scss";
+import { useTranslation } from "react-i18next";
 
 const Rules = () => {
   const history = useHistory();
+  const { t } = useTranslation();
 
   function handleClick() {
     history.push("/lobbies");
   }
   return (
     <div className="rules">
-      <h1>Game Rules</h1>
+      <h1>{t("rules.rules")}</h1>
       <div className="rules-text">
-        <h3>Game Description</h3>
+        <h3>{t("rules.description.title")}</h3>
         <ul className="list">
           <li>
-            Pictionary is a multiplayer drawing game, where a painter draws a
-            word and the other players try to guess the word. The game can be
-            played in a <span>users preferred language</span> due to the usage
-            of the <span>Google Translate API</span>, which translates all words
-            into the users preferred language.
+            {t("rules.description.game.part1.1")}
+            <span>{t("rules.description.language")}</span>
+            {t("rules.description.game.part1.2")}
+            <span>{t("rules.description.API")}</span>
+            {t("rules.description.game.part1.3")}
           </li>
           <li>
-            A game can be played with <span>2-5 players</span>
+            {t("rules.description.game.part2")}
+            <span>{t("rules.description.players")}</span>
           </li>
           <li>
-            A game consists of <span>1-5 rounds</span>. A round is over, when
-            every player has drawn once. (number of turns per round = number of
-            players in lobby)
+            {t("rules.description.game.part3.1")}
+            <span> {t("rules.description.rounds")}</span>.{" "}
+            {t("rules.description.game.part3.2")}
           </li>
           <li>
-            The duration of a turn can be choosen from{" "}
-            <span>20-60 seconds</span>
+            {t("rules.description.game.part4")}
+
+            <span> {t("rules.description.seconds")}</span>
           </li>
           <li>
-            A turn is finished, either when the <span>time is over</span>,{" "}
-            <span>all players have guessed</span> or the
-            <span> painter was inactive.</span>
+            {t("rules.description.game.part5.1")}
+            <span> {t("rules.description.game.part5.2")}</span>
           </li>
           <li>
-            A guess can be submitted in the{" "}
-            <span>users preferred language</span>.
+            {t("rules.description.game.part6")}
+            <span> {t("rules.description.language")}</span>
+            {t("rules.description.game.part6-1")}
           </li>
         </ul>
-        <h3>Rules</h3>
+        <h3>{t("rules.gameRules.title")}</h3>
         <ul className="list">
-          <li>One guess can be submitted per turn</li>
-          <li>Behave respectfully</li>
+          <li> {t("rules.gameRules.rules.part1")}</li>
+          <li> {t("rules.gameRules.rules.part2")}</li>
           <li>
-            <span>Don't</span> use letters in your drawings.
+            <span>{t("rules.gameRules.rules.part3.1")}</span>{" "}
+            {t("rules.gameRules.rules.part3.2")}
           </li>
         </ul>
-        <h3>Scoring</h3>
+        <h3>{t("rules.scoringRules.title")}</h3>
         <ul className="list">
           <li>
-            The painter gets <span>5 points</span> for every right guess
+            {t("rules.scoringRules.rules.part1.1")}
+            <span> {t("rules.scoringRules.5_points")}</span>
+            {t("rules.scoringRules.rules.part1.2")}
           </li>
-          <li>If a players guesses the word wrong, the player gets 0 points</li>
           <li>
-            The first player, that guesses correctly, gets 25 points. Each
-            correct guess that was submitted later, gets 5 points less. For
-            example: First correct guess 25 points, 2nd correct guess 20 points
-            etc..{" "}
+            {" "}
+            {t("rules.scoringRules.rules.part2")}
+            <span> {t("rules.scoringRules.0_points")}</span>
           </li>
+          <li>{t("rules.scoringRules.rules.part3")}</li>
         </ul>
       </div>
       <button className="back-button" onClick={handleClick}>
-        BACK
+        {t("rules.back")}
       </button>
     </div>
   );
